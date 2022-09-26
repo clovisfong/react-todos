aa07a7e71ab458eaeb50dbbc708a60e9b7584c3d
Clovis Fong <fongclovis@hotmail.com>
2022-09-26 11:59:45 +0800

    added client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TodosNew from "./routes/TodosNew";
+
+// no longer rely on react
+const TodosLoader = async () => {
+  const res = await fetch("/api/todos/");
+  const data = await res.json();
+  return data;
+};
+
+//New Method 1
+const router = createBrowserRouter([
+  { path: "/", loader: TodosLoader, element: <TodosNew /> },
+]);
+
+//New Method 2
+const router2 = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" loader={TodosLoader} element={<TodosNew />} />
+  )
+);
+
 function App() {
   return (
-    <div className="App">
-      <h1>React Todos</h1>
-    </div>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Todos />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const { faker } = require('@faker-js/faker')
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 // added this 
 app.use(express.static("./client/dist/"));
 
-app.get("/api/", (req, res) => {
-    res.send("Hello World!");
+const todos = [1, 2, 3].map(() => {
+    return { title: faker.company.name() };
 });
+
+app.get("/api/todos", (req, res) => {
+    res.json(todos);
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
